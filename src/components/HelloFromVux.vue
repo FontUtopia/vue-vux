92a223016ac90597067eeac10b80b7f0eadb502a
FontUtopia <niuchaoying123@163.com>
2017-03-15 19:23:59 +0800

    init project

---
+++ src/components/HelloFromVux.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <div class="vux-demo">
+      <img class="logo" src="../assets/vux_logo.png">
+      <h1> </h1>
+    </div>
+    <group title="cell demo">
+      <cell title="Vux" value="Cool" is-link></cell>
+    </group>
+    <div class="box">
+      <checker v-model="demo1" default-item-class="demo1-item" selected-item-class="demo1-item-selected">
+      <checker-item value="1">潘</checker-item>
+      <checker-item value="2">闲</checker-item>
+      <checker-item value="3">邓</checker-item>
+      </checker>
+      <br>
+      <span>当前值: {{demo1}}</span>
+      <br>
+    </div>
+  </div>
+</template>
+
+<script>
+import { Group, Cell, Checker,CheckerItem} from 'vux'
+
+export default {
+  components: {
+    Group,
+    Cell,
+    Checker,
+    CheckerItem
+  },
+  data () {
+    return {
+      // note: changing this line won't causes changes
+      // with hot-reload because the reloaded component
+      // preserves its current state and we are modifying
+      // its initial state.
+      msg: 'Hello World!',
+      items1: [{
+        key: '1',
+        value: 'A'
+      }, {
+        key: '2',
+        value: 'B'
+      }, {
+        key: '3',
+        value: 'C'
+      }],
+      demo1: ''
+    }
+  },
+  method:{
+    
+  }
+}
+</script>
+
+<style>
+.vux-demo {
+  text-align: center;
+}
+.logo {
+  width: 100px;
+  height: 100px
+}
+.demo1-item {
+  border: 1px solid #ececec;
+  padding: 5px 15px;
+}
+.demo1-item-selected {
+  border: 1px solid green;
+}
+</style>
